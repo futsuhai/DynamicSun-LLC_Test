ecf7dfd5a1bb5a6fe11f4a94617589b1118f98e7
futsuhai <130190351+futsuhai@users.noreply.github.com>
2024-03-10 22:39:26 +0300

    ui refactoring(in progress)

--- Weather-client/src/app/app.config.ts
+++ Weather-client/src/app/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(APP_ROUTES),
     importProvidersFrom(HttpClientModule),
-  ]
+    importProvidersFrom(MatNativeDateModule ),
+    provideAnimations()
+]
 };
--- Weather-client/src/app/components/pages/main-page/main-page.component.ts
+++ Weather-client/src/app/components/pages/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherService } from 'src/app/services/weather.service';
 import { IWeather } from 'src/app/models/Weather.model';
@@ -6,40 +6,33 @@ import { IWeatherDate } from 'src/app/models/WeatherDate.model';
 import { format } from 'date-fns';
 import { ru } from 'date-fns/locale';
 import { IWeatherParams } from 'src/app/models/WeatherParams.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatButtonModule],
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
   host: {
     class: 'main-page-component'
   }
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  public testDate: IWeatherDate = {
-    date: new Date(Date.UTC(2010, 0, 4, 0, 0, 0))
+  public selectedDate: IWeatherDate = {
+    date: new Date()
   };
   public weathers: IWeather[] = [];
-  public testWeathers: IWeather[] = [];
   public activeWeatherIndex: number = 0;
-
   public weatherParams: IWeatherParams[] = [];
 
   constructor(private weatherService: WeatherService) { }
-
-  public ngOnInit(): void {
-    this.weatherService.getWeatherWithDate(this.testDate).subscribe({
-      next: (weathers: IWeather[]) => {
-        if (weathers) {
-          this.weathers = weathers;
-          this.setWeatherParams(this.weathers[this.activeWeatherIndex]);
-        }
-      }
-    });
-  }
 
   public toggleActive(index: number) {
     this.activeWeatherIndex = index;
@@ -50,12 +43,25 @@ export class MainPageComponent implements OnInit {
     return format(date, 'd MMMM yyyy', { locale: ru });
   }
 
+  public selectDate(): void {
+    const utcDate = new Date(Date.UTC(this.selectedDate.date.getFullYear(), this.selectedDate.date.getUTCMonth(), this.selectedDate.date.getUTCDate() + 1, 0, 0, 0));
+    this.selectedDate.date = utcDate;
+    this.weatherService.getWeatherWithDate(this.selectedDate).subscribe({
+      next: (weathers: IWeather[]) => {
+        if (weathers) {
+          this.weathers = weathers;
+          this.setWeatherParams(this.weathers[this.activeWeatherIndex]);
+        }
+      }
+    });
+  }
+
   private setWeatherParams(activeWeather: IWeather) {
     this.weatherParams = [
       { name: 'Влажность, %', value: activeWeather.humidity },
       { name: 'Точка росы, гр. Ц.', value: activeWeather.td },
       { name: 'Давление, мм рт. ст.', value: activeWeather.pressure },
-      { name: 'Направление ветра', value: activeWeather.windDirection },
+      { name: 'Направление ветра', value: activeWeather.windDirection === " " ? '-' : activeWeather.windDirection },
       { name: 'Скорость ветра, м/c', value: activeWeather.windSpeed === 0 ? '-' : activeWeather.windSpeed },
       { name: 'Облачность, %', value: activeWeather.cloudy === 0 ? '-' : activeWeather.cloudy },
       { name: 'Нижняя граница облачности, м', value: activeWeather.h },
